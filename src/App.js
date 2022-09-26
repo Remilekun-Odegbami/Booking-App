68a601f6b932135d664482100fd6f94622cd6802
Remilekun Odegbami <remdev22@gmail.com>
2022-09-26 21:49:48 +0100

    working on single hotel page

--- src/App.js
+++ src/App.js
@@ -17,5 +17,3 @@ function App() {
 }
 
 export default App;
-
-//1:56:43
--- src/pages/singleHotel/SingleHotel.jsx
+++ src/pages/singleHotel/SingleHotel.jsx
@@ -12,12 +12,13 @@ import "./SingleHotel.css"
 
 export default function SingleHotel() {
     const location = useLocation()
+    console.log(location)
     const hotel_id = location.pathname.split("/"[2])
     console.log(hotel_id)
     const [slideNum, setSlideNum] = useState(0)
     const [openImage, setOpenImage] = useState(false)
 
-    const {data, loading, error} = useFetch(`hotels/${hotel_id}`)
+    const { data, loading } = useFetch(`/api/hotels/${hotel_id}`);
 
     const images = [
         {
@@ -77,16 +78,16 @@ export default function SingleHotel() {
                         <FontAwesomeIcon className='close-btn' icon={faCircleXmark} onClick={()=> setOpenImage(false)} />
                         <FontAwesomeIcon className='arrow' icon={faCircleArrowLeft} onClick={()=> handleMove("l")} />
                         <div className="slider-wrapper">
-                            <img src={images[slideNum].src} alt="" className='img-fluid' />
+                            <img src={data.imgs} alt="" className='img-fluid' />
                         </div>
                         <FontAwesomeIcon className='arrow' icon={faCircleArrowRight} onClick={()=> handleMove("r")} />
                     </div>}
                     <div className="col-md-9 col-sm-12">
-                        <h2 className="single-hotel-header">Tower Street Appartments</h2>
+                        <h2 className="single-hotel-header">{data.name}</h2>
                         <p className="location"><i className="fa fa-map-marker" aria-hidden="true"></i> {" "}
-                        5 Tafawa Balewa, Plateau State </p>
-                        <p className="distance">Excellent location - 500m from center</p>
-                        <p className="promo">Book a stay with N150,000 at this property and get a free airport taxi</p>
+                        {data.address} </p>
+                        <p className="distance">Excellent location - {data.distance}m from center</p>
+                        <p className="promo">Book a stay with &#8358;{data.cheapestPrice} at this {data.type} and get a free airport taxi</p>
                     </div>
                     <div className="col-md-3 col-sm-12">
                         <button className='btn'>Reserve or Book Now!</button>
@@ -95,7 +96,7 @@ export default function SingleHotel() {
                         {images.map((image, index) => (
                             <div className="col-md-4 col-sm-12 my-3" key={image.id}>
                                 <img 
-                                    src={image.src} 
+                                    src={data.imgs[index]} 
                                     alt=""
                                     className='img-fluid'
                                     onClick={()=>handleOpen(index)}
@@ -104,15 +105,15 @@ export default function SingleHotel() {
                         ))}
                     </div>
                     <div className="hotel-about col-md-8 col-sm-12">
-                        <h3 className="info">Stay in the heart of Plateau</h3>
+                        <h3 className="info">{data.title}</h3>
                         <p className="more-info">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo, aperiam reiciendis! Alias perferendis adipisci sit, quod voluptatibus illum non eaque ratione maiores. Suscipit modi natus rem recusandae praesentium perferendis dignissimos voluptate repudiandae soluta delectus, omnis necessitatibus voluptas iste unde dolore vel quia molestias assumenda ut ipsam officiis numquam nulla cum! Omnis possimus ipsa quae, odit sapiente harum consequatur perspiciatis! Saepe ullam quisquam inventore, ipsam repudiandae dolorum facilis, voluptatum fugiat tempore incidunt id ea vel. Eligendi obcaecati mollitia accusantium recusandae magnam consequatur! At, vero dicta. Facere voluptatibus dolorum sapiente vel, impedit, veritatis aspernatur totam, explicabo culpa corrupti vitae! Cum, amet suscipit nulla sequi quae nesciunt maxime quos omnis eaque? Voluptatibus incidunt architecto excepturi dolore, sunt, facere voluptatum, quod tempore perferendis id voluptate ab ratione eos quis inventore culpa ducimus alias! Maiores voluptas numquam accusamus.
+                            {data.desc}
                         </p>
                     </div>
                     <div className="col-md-4 col-sm-12 aside">
                         <h4 className="perfect">Perfect for a 9 night stay!</h4>
-                        <p className="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui asperiores dignissimos, accusantium porro hic id!</p>
-                        <h4 className="amount">&#8358;400,000 <span>(7 nights)</span></h4>
+                        <p className="text">{data.name} - {data.title}</p>
+                        <h4 className="amount">&#8358;{data.cheapestPrice * 9} <span>(9 nights)</span></h4>
                         <button className='btn'>Reserve or Book Now!</button>
                     </div>
                 </div>
